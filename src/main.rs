89e756c2cfc81d75d04c091c67f0314afa994340
PCDSandwichMan <46288251+PCDSandwichMan@users.noreply.github.com>
2024-04-21 00:00:00 +0000

    feat(root): 0365F5A1-1BFC-44B1-810F-E344C076BB58

--- src/main.rs
+++ src/main.rs
@@ -18,6 +18,29 @@ use enigo::{Direction::Click, Enigo, Key, Keyboard, Settings};
 //         .expect("failed to execute process");
 // }
 
+// pub fn execute_command(command: &str) {
+//     println!("Running command: {}", command);
+//     // focus_window();
+
+//     println!("Executing command");
+
+//     let mut enigo = Enigo::new(&Settings::default()).unwrap();
+
+//     match command {
+//         "a" => enigo.key(Key::Unicode('z'), Click).unwrap(),
+//         "b" => enigo.key(Key::Unicode('x'), Click).unwrap(),
+//         "y" => enigo.key(Key::Unicode('c'), Click).unwrap(),
+//         "x" => enigo.key(Key::Unicode('v'), Click).unwrap(),
+//         "up" => enigo.key(Key::Unicode('w'), Click).unwrap(),
+//         "down" => enigo.key(Key::Unicode('s'), Click).unwrap(),
+//         "left" => enigo.key(Key::Unicode('a'), Click).unwrap(),
+//         "right" => enigo.key(Key::Unicode('d'), Click).unwrap(),
+//         _ => (),
+//     }
+
+//     println!("Command executed");
+// }
+
 pub fn execute_command(command: &str) {
     println!("Running command: {}", command);
     // focus_window();
@@ -27,14 +50,19 @@ pub fn execute_command(command: &str) {
     let mut enigo = Enigo::new(&Settings::default()).unwrap();
 
     match command {
-        "a" => enigo.key(Key::Unicode('z'), Click).unwrap(),
-        "b" => enigo.key(Key::Unicode('x'), Click).unwrap(),
-        "y" => enigo.key(Key::Unicode('c'), Click).unwrap(),
-        "x" => enigo.key(Key::Unicode('v'), Click).unwrap(),
-        "up" => enigo.key(Key::Unicode('w'), Click).unwrap(),
-        "down" => enigo.key(Key::Unicode('s'), Click).unwrap(),
-        "left" => enigo.key(Key::Unicode('a'), Click).unwrap(),
-        "right" => enigo.key(Key::Unicode('d'), Click).unwrap(),
+        // Buttons
+        "a" => enigo.key(Key::Unicode('x'), Click).unwrap(),
+        "b" => enigo.key(Key::Unicode('z'), Click).unwrap(),
+        "y" => enigo.key(Key::Unicode('a'), Click).unwrap(),
+        "x" => enigo.key(Key::Unicode('s'), Click).unwrap(),
+
+        // Directions
+        "up" => enigo.key(Key::UpArrow, Click).unwrap(),
+        "down" => enigo.key(Key::DownArrow, Click).unwrap(),
+        "left" => enigo.key(Key::LeftArrow, Click).unwrap(),
+        "right" => enigo.key(Key::RightArrow, Click).unwrap(),
+
+        // Fallback
         _ => (),
     }
 
