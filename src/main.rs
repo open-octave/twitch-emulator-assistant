76eacfd66fa42471eee7de3cd2dc493c6c12ac51
PCDSandwichMan <46288251+PCDSandwichMan@users.noreply.github.com>
2024-04-21 00:00:00 +0000

    feat(root): 5BEA3488-D467-4E25-99CA-B37382E417D4

--- src/main.rs
+++ src/main.rs
@@ -6,7 +6,40 @@ use twitch_irc::{ClientConfig, SecureTCPTransport};
 use std::io;
 use std::io::Write;
 
-mod win_impl;
+use enigo::{Direction::Click, Enigo, Key, Keyboard, Settings};
+
+// pub fn focus_window() {
+//     use std::process::Command;
+
+//     Command::new("osascript")
+//         .arg("-e")
+//         .arg("tell application \"RetroArch\" to activate")
+//         .output()
+//         .expect("failed to execute process");
+// }
+
+pub fn execute_command(command: &str) {
+    println!("Running command: {}", command);
+    // focus_window();
+
+    println!("Executing command");
+
+    let mut enigo = Enigo::new(&Settings::default()).unwrap();
+
+    match command {
+        "a" => enigo.key(Key::Unicode('z'), Click).unwrap(),
+        "b" => enigo.key(Key::Unicode('x'), Click).unwrap(),
+        "y" => enigo.key(Key::Unicode('c'), Click).unwrap(),
+        "x" => enigo.key(Key::Unicode('v'), Click).unwrap(),
+        "up" => enigo.key(Key::Unicode('w'), Click).unwrap(),
+        "down" => enigo.key(Key::Unicode('s'), Click).unwrap(),
+        "left" => enigo.key(Key::Unicode('a'), Click).unwrap(),
+        "right" => enigo.key(Key::Unicode('d'), Click).unwrap(),
+        _ => (),
+    }
+
+    println!("Command executed");
+}
 
 #[tokio::main]
 pub async fn main() {
@@ -32,7 +65,7 @@ pub async fn main() {
                     // If the command is a valid command, execute it
                     match command.as_str() {
                         "a" | "b" | "x" | "y" | "up" | "down" | "left" | "right" => {
-                            win_impl::execute_command(command.trim());
+                            execute_command(command.trim());
                         }
                         _ => {}
                     }
